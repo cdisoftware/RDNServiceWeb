a44546f7b36039906a5e4adc40abfff345131331
Juan José Gutiérrez Leguizamón <68406983+JuanGut06@users.noreply.github.com>
2022-03-24 12:13:40 -0500

    AJUSTES

---
+++ src/main/java/com/cdi/com/CorferiasRuedaNegocios/Services/PTtListaPaisService.java
@@ -0,0 +1,9 @@
+package com.cdi.com.CorferiasRuedaNegocios.Services;
+
+import com.cdi.com.CorferiasRuedaNegocios.Entity.PTtListaPaisEntity;
+import java.util.List;
+
+public interface PTtListaPaisService {
+
+    public List<PTtListaPaisEntity> ConsultaPtListPais();
+}
